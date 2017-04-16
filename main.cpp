211e4a896a0e40057fc3502ae4e83ba40519b880
mgomez101 <mgomez101@student.rcc.edu>
2017-04-15 17:43:16 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,60 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 /* 
  * File:   main.cpp
  * Author: Michelle Gomez
- * Created on March 14, 2017, 11:29 AM
- * Purpose:  Time Calculator converter
+ *
+ * Created on February 17, 2017, 12:53 PM
  */
 
-//System Libraries Here
+
 #include <iostream>
+
+
 using namespace std;
 
-//User Libraries Here
 
-//Global Constants Only, No Global Variables
-//Like PI, e, Gravity, or conversions
-const int MINUTE = 60;        //How many seconds in a minute
-const int HOUR = 60 * MINUTE; //How many seconds in an hour
-const int DAY = 24 * HOUR;    //How many seconds in a day
-const int WEEK = 7 * DAY;     //How many seconds in a week
-const int YEAR = 52 * WEEK;   //How many seconds in a year
-const int MONTH = WEEK * (13/3);  //How many seconds in an average year
-//Function Prototypes Here
+void menu(void);
+void game1(void);
+void game2(void);
+void game3(void);
 
-//Program Execution Begins Here
-int main(int argc, char** argv) {
-    //Declare all Variables Here
-    int nSecs; //Number of seconds to convert
-    int nYrs, nMnths, nWks, nDys, nHrs, nMins;
-    //Input or initialize values Here
-    cout<<"This program converts seconds to years/months/weeks/days/hours"<<endl;
-    cout<<"Input number of seconds for the conversion/equivalence"<<endl;
-    cin>>nSecs;
+int main() {
+
+    //Declare variables
+    char choice;
     
-    //Process/Calculations Here
-    nYrs = nSecs/YEAR;      //Number of years
-    cout<<nYrs<<" years ";
-    nSecs -= nYrs*YEAR;     //Subtract yearly seconds
-    
-    nWks = nSecs/WEEK;      //Number of weeks
-    cout<<nWks<<" weeks ";
-    nMnths = nWks * 3/13;   //Convert number of weeks to months
-    cout<<nMnths<<" months ";
-    nSecs -= nWks*WEEK;
-    nDys = nSecs/DAY;
-    cout<<nDys<<" days ";
-    nSecs -= nDys*DAY;
-    nHrs = nSecs/HOUR;
-    cout<<nHrs<<" hours ";
-    nSecs -= nHrs*HOUR;
-    nMins = nSecs/MINUTE;
-    cout<<nMins<<" minutes ";
-    nSecs -= nMins*MINUTE;
-    cout<<nSecs<<" seconds";
-    //Output Located Here
-   
+    //Show menu and loop
+    do
+    {
+        //Display Menu
+        menu();
+        
+        //Input choice 
+        cout<<"Game # ";
+        cin>>choice;
+        
+        switch(choice)
+        {
+            case '1': game1();break;
+            case '2': game2();break;
+            case '3': game3();break;
+            default: 
+            {
+                cout<<"Thanks for not playing"<<endl;
+            }
+        }
+    }
+    while(choice>='1' && choice<='3');
 
-    //Exit
     return 0;
 }
 
+void game3(void)
+{
+    cout<<" GAME ONE / MASTERMIND / EASY "<<endl<<endl;
+    
+    int winner = 817;
+    int num;
+    int correct;
+    int guess;
+    int cNumber;
+    int cPlace;
+    int cLeft, cMid,cRight; //Correct numbers
+    int gLeft, gMid, gRight; //Guessed numbers
+    
+    cLeft = winner/100;
+    cMid = (winner/10)%10;
+    cRight = winner%10;
+    
+    num=1, correct=0;
+    
+    while(!correct)
+    {
+        cout<<"Guess my number (between 0 and 999 of course)"<<endl;
+        cout<<"This is GUESS #"<<num;
+        cin>>guess;
+        
+        if (guess>=0 && guess <=999)
+        {
+            if (guess == winner)
+            {
+                cout<<"WOW. That took you "<<num<<" move(s)!"<<endl;
+                correct=1;
+            }
+            else
+            {
+                gLeft = guess/100;
+                gMid = (guess/10) % 10;
+                gRight = guess%10;
+                
+                cNumber = 0;
+                if (gLeft == cLeft || gLeft == cMid || gLeft == cRight) cNumber++;
+                if (gMid == cLeft || gMid == cMid || gMid == cRight) cNumber++;
+                if (gRight == cLeft || gRight == cMid || gRight == cRight) cNumber++;
+                
+                
+                cPlace = 0;
+                if (gLeft == cLeft) cNumber++;
+                if (gMid == cMid) cNumber++;
+                if (gRight == cRight) cNumber++;
+                
+                cout<<"Correct Order: "<<cPlace<<endl;
+                cout<<"Correct Digits: "<<cNumber<<endl; 
+            }
+        } else
+        {
+            cout<<"FOLLOW INSTRUCTIONS.";
+        }
+        num++;
+    }
+    
+}
+
+void game2(void)
+{
+    cout<<" GAME TWO / / "<<endl;
+    
+}
+
+void game1(void)
+{
+    cout<<" GAME THREE / / "<<endl;
+}
+
+void menu(void)
+{
+    cout<<endl<<endl<<"************WELCOME******************"<<endl;
+    cout<<"Type 0 to Exit"<<endl;
+    cout<<"Type 1 for Game One"<<endl;
+    cout<<"Type 2 for Game Two"<<endl;
+    cout<<"Type 3 for Game Three"<<endl;
+    
+}
